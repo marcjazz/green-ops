dc7d94574899217d7b4d40484e2cd297f8bcb972
Marco Kuidja <kuidjamarco@gmail.com>
2026-06-03 00:15:18 +0200

    Configure HTTPS with self-signed certificates

--- apps/frontend/src/auth/userManager.ts
+++ apps/frontend/src/auth/userManager.ts
@@ -1,9 +1,9 @@
 import { UserManager } from 'oidc-client-ts';
 
 const oidcConfig = {
-  authority: 'http://accounts.greenops.local/realms/greenops',
+  authority: 'https://accounts.greenops.local/realms/greenops',
   client_id: 'greenops-frontend',
-  redirect_uri: 'http://greenops.local/',
+  redirect_uri: 'https://greenops.local/',
 };
 
 export const userManager = new UserManager(oidcConfig);
