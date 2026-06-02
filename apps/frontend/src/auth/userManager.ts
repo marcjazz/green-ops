aced13f2cca44c758ba0a401f7fb1361d7cb308f
Marco Kuidja <kuidjamarco@gmail.com>
2026-06-03 00:05:29 +0200

    Automate Keycloak realm import and fix OIDC realm

--- apps/frontend/src/auth/userManager.ts
+++ apps/frontend/src/auth/userManager.ts
@@ -1,7 +1,7 @@
 import { UserManager } from 'oidc-client-ts';
 
 const oidcConfig = {
-  authority: 'http://accounts.greenops.local/realms/master',
+  authority: 'http://accounts.greenops.local/realms/greenops',
   client_id: 'greenops-frontend',
   redirect_uri: 'http://greenops.local/',
 };
